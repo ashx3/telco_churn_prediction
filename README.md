b33cf12c176c6bb8574decf9853fccec30142454
ashx3 <iashik0506@gmail.com>
2022-05-26 03:31:19 +0530

    Adding files from local repo

---
+++ README.md
@@ -0,0 +1,86 @@
+## Telecom Retention Using Data Science to address Customer Churn
+
+# Data Source
+KAGGLE - https://www.kaggle.com/datasets/blastchar/telco-customer-churn
+
+# Context
+The most common issue that any company faces is to retain their customers, be it gaming, food, banking or a telecom industry, the issue pertains to be the same. These companies invest in lump sum in order to retain their customers and it this proves to be a challenging task.
+
+![customer churn](/images/customer_churn.jpg)
+
+# Content
+Here we will analyzing the customer retention use case which will include all the customer behaviour and attributes that will be useful in identifying the characteristics of the churners. Such characteristics or insights will help in building a good model.
+
+# Attribute Information
+
+# Identifier
+- customerID - ID number of the customer
+
+# Churn
+- Churn - Churn status, whether the customer churned or not
+
+# Demographic information
+- gender - Whether the customer is a male or female
+- SeniorCitizen - Whether the customer is a senior citizen or not
+- Partner - Whether the customer has a partner or not
+- Dependents - Whether the customer has dependents or not
+
+# Customer account information
+- tenure - Number of months the customer has used the service
+- Contract - The contract term of the customer
+- PaperlessBilling - Whether the customer has paperless billing or not
+- PaymentMethod - The customer's payment method
+- MonthlyCharges - The amount charged to the customer monthly
+- TotalCharges - The total amount charged to the customer
+
+# Services that each customer has signed up for
+- PhoneService - Whether the customer has a phone service or not
+- MultipleLines - Whether the customer has multiple lines or not
+- InternetService - Customer's internet service provider
+- OnlineSecurity - Whether the customer has online security or not
+- OnlineBackup - Whether the customer has online backup or not
+- DeviceProtection - Whether the customer has device protection or not
+- TechSupport - Whether the customer has tech support or not
+- StreamingTV - Whether the customer has streaming TV or not
+- StreamingMovies - Whether the customer has streaming movies or not
+
+# Company Goals
+__Increasing profit! But how can we achieve it? Some of the way to increase profit are:__
+- Acquiring new customers as much as we can
+- Retaining existing customers as much as we can
+
+![types of customers](/images/customers_types.jpg)
+
+# Problems
+- Companies need to invest (expense costs) to get new customers
+- When a customer leaves the service (churns), it indicates a loss of investment
+- Cost, time and effort need to be channelled to replace customers who have left the service
+- Acquiring new customers is often more difficult and more expensive than retaining customers
+- On Harvard Business Review page, they said: acquiring a new customer is anywhere from 5 to 25 times more expensive than retaining an existing one
+
+![Harvard Business Review reference page](/images/harvard_business_review.jpg)
+
+# Objectives
+- Predict whether customers will continue to use the service or will leave the service
+- Understanding the customer behaviours:
+ - What keeps customers using the service
+ - What makes customers leave the service
+
+In this repository, we have performed the end to end Exploratory Data Analysis and identified the characteristics of the customers that are more likely to churn and trained a model, and lately we have deployed the model using streamlit.
+
+### 🟢 For EDA, please refer to : Telco_Churn_Analysis_EDA.ipynb
+### 🟢 For Model Building, please refer to : Telco_Churn_Analysis_Model Building.ipynb
+### 🟢 For Model Deployment, please refer to app.py
+
+# How to start streamlit?
+
+Simply run the below command on the terminal and let it do the magic
+```
+streamlit run app.py
+```
+
+__This is how our Streamlit app looks:__
+
+![streamlitapp page1](/images/app1)
+
+![streamlitapp page2](/images/app2)
---
+++ app.py
@@ -0,0 +1,128 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import warnings
+warnings.filterwarnings('ignore')
+
+data = pd.read_csv('D:/Portfolio/Churn/tel_churn.csv')
+
+
+st.title('Telecom Churn Prediction App')
+
+name = st.text_input('Enter your name:', key='name')
+
+st.subheader(f'Hi {name},')
+
+st.markdown(f'''The Telecom Churn Prediction App is used to detect whether a Customer will Churn or not Churn from his Telecom provider. 
+            Here, the user has to select the relevant options below to generate results. The generated results are predicted by the 
+            trained model with confidence levels, these predictions are made after training the model with the attributes and 
+            characteristics of Churners & Non-Churners.''')
+
+gender = st.radio('Select your Gender:', ('Male', 'Female'))
+
+senior_citizen = st.radio('Are you a Senior Citizen?', ('No', 'Yes'))
+
+partner = st.radio('Do you have a partner?', ('Yes', 'No'))
+
+dependents = st.radio('Do you have any dependents?', ('Yes', 'No'))
+
+phone_service = st.radio('Do you use phone services?', ('Yes', 'No'))
+
+multiple_lines = st.radio('Do you use multiple lines?', ('Yes', 'No'))
+
+internet_service = st.radio('What mode do you use to access internet services?', ( 'DSL', 'Fiber optic','No'))
+
+online_security = st.radio('Do you use online security?', ('Yes', 'No'))
+
+online_backup = st.radio('Do you use online backup?', ('Yes', 'No'))
+
+device_protection = st.radio('Do you use device protection?', ('Yes', 'No'))
+
+tech_support = st.radio('Do you use tech support?', ('Yes', 'No'))
+
+streaming_tv = st.radio('Do you use streaming services on TV?', ('Yes', 'No'))
+
+streaming_movies = st.radio('Do you steam movies?', ('Yes', 'No'))
+
+paperless_billing = st.radio('Do you use paperless billing?', ('Yes', 'No'))
+
+contract = st.selectbox("What type of Contract Term are you subscribed?",
+                         ['Month-to-month', 'One year', 'Two year'])
+ 
+st.success(f"Contract Type - {contract}")
+
+monthly_charges = st.slider('Monthly Charges (USD)', max(data["MonthlyCharges"]), min(data['MonthlyCharges']))
+
+total_charges = st.slider('Total Charges (USD)', max(data["TotalCharges"]), min(data['TotalCharges']))
+
+payment_method = st.selectbox("What type of payment method do you use :",
+                         ['Electronic check', 'Mailed check', 'Bank transfer (automatic)',
+       'Credit card (automatic)'])
+
+tenure_group = st.selectbox("How many months have you been using the services?", 
+                         ['1 - 12', '13 - 24', '25 - 36', '37 - 48', '61 - 72', '49 - 60'], help = 'Tenure')
+
+
+input = [[gender, senior_citizen, partner, dependents, phone_service, multiple_lines, internet_service, online_security,
+            online_backup, device_protection, tech_support, streaming_tv, streaming_movies, contract, paperless_billing, 
+            payment_method, monthly_charges, total_charges, tenure_group]]
+
+cols = ['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'PhoneService',
+       'MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup',
+       'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies',
+       'Contract', 'PaperlessBilling', 'PaymentMethod', 'MonthlyCharges',
+       'TotalCharges', 'tenure_group']
+
+predictor = pd.DataFrame(input, columns=cols)
+
+def preprocess_predictor(data):
+    # loading encoders
+    with open('encoders\StandardScaler', 'rb') as f:
+        ss = pickle.load(f)
+
+    with open('encoders\One-Hot-Encoder', 'rb') as f:
+        ohe = pickle.load(f)
+
+    # categorical columns
+    cat_cols = [col for col in data.columns if data[col].dtype == 'object']
+
+    # numerical columns
+    num_cols = [col for col in data.columns if data[col].dtype != 'object']
+
+    # standard scaler for predictor
+    data[num_cols] = ss.transform(data[num_cols])
+
+    # one-hot-encoding for predictor
+    data_ohe = ohe.transform(data[cat_cols])
+
+    col_ohe = ohe.get_feature_names(cat_cols)
+
+    data_ohe_df = pd.DataFrame(data_ohe, columns = col_ohe, index = data.index)
+
+    data_final = pd.concat([data.drop(columns=cat_cols), data_ohe_df], axis=1)
+
+    with open('model1.sav', 'rb') as f:
+        model = pickle.load(f)
+
+    single = model.predict(data_final)
+    probability = model.predict_proba(data_final)
+
+    if single == 1:
+        st.error("The customer is likely to be Churn!!")
+        st.warning("Confidence = {}".format(probability[:,1] * 100) + '%')
+    else:
+        st.success("The customer is likely to continue!!")
+        st.warning("Confidence = {}".format(probability[:, 0] * 100) + '%')
+
+if(st.button('Predict')):
+    preprocess_predictor(predictor)
+
+
+# remove main menu and footer note
+hide_menu_style = '''
+       <style>
+       #MainMenu {visibility: hidden;}
+       footer{visibility: hidden;}
+       </style>
+        '''
+st.markdown(hide_menu_style, unsafe_allow_html=True)
